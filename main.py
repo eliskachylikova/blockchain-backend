0faee3bd1ba4ce34fcfacb7758b9b3604031e458
Eliška Chylíková <xchyliko@mendelu.cz>
2023-02-17 12:18:19 +0100

    Improved storing data

--- main.py
+++ main.py
@@ -16,10 +16,30 @@ contract_address = '0xD8d1546c838091fDC3a01741eBfe1E8133560ecd'
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
 
+# Load the account from a private key
+private_key = '450f187df076b5021adffec0f5a5aea216bd492c615d7a0be1fbfb788cf25522'
+account = w3.eth.account.from_key(private_key)
+w3.eth.defaultAccount = account.address  # Set the account as the default account
+
+
 @app.get("/store")
-async def store(number: int):
-    # Call the smart contract function to save the data
-    tx_hash = contract.functions.store(number).transact()
+async def store(num: int):
+    print(num)
+
+    # Construct the transaction object
+    #nonce = w3.eth.getTransactionCount(w3.eth.defaultAccount)
+    tx = {
+        'from': w3.eth.defaultAccount,
+        'to': contract_address,
+        'gas': 2000000,
+        'gasPrice': 1000,
+        'nonce': 2,
+        'data': contract.functions.store(5)#.encodeABI()
+    }
+
+    # Sign and send the transaction
+    signed_tx = account.signTransaction(tx)
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
     # Wait for the transaction to be mined
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
@@ -27,11 +47,19 @@ async def store(number: int):
     # Return the transaction receipt to the client
     return receipt
 
+    #
+    # # Call the smart contract function to save the data
+    # tx_hash = contract.functions.store(number).transact()
+    #
+    # # Wait for the transaction to be mined
+    # receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    #
+    # # Return the transaction receipt to the client
+    # return receipt
+
 
 @app.get("/")
 async def root():
-    # print(w3.eth.get_block('latest'))
-    # return {"message": w3.eth.get_block('latest')}
     return {"message": "Hello"}
 
 

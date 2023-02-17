8df2938f52de65ccedc92abeef8cec4b4d391260
Eliška Chylíková <xchyliko@mendelu.cz>
2023-02-17 17:10:48 +0100

    Check endpoints

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ account = w3.eth.account.from_key(private_key)
 w3.eth.defaultAccount = account.address  # Set the account as the default account
 
 
-@app.get("/assign_permission", tags=['Permissions'], summary="Assign permission for a user to control devices in a room")
-async def assign_permission(user_id: int, room_id: int):
+@app.get("/assign_user_permission", tags=['Permissions'], summary="Assign permission for a user to control devices in a room")
+async def assign_user_permission(user_id: int, room_id: int):
     tx = contract.functions.addUserPermission(user_id, room_id).build_transaction({
         'gas': 200000,
-        'gasPrice': w3.toWei('1.05', 'gwei'),
+        'gasPrice': w3.to_wei('1.05', 'gwei'),
         'nonce': w3.eth.get_transaction_count(w3.eth.defaultAccount),
     })
 
@@ -33,6 +33,31 @@ async def assign_permission(user_id: int, room_id: int):
     t = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
     print(t.hex())
+
+
+@app.get("/assign_group_permission", tags=['Permissions'], summary="Assign permission for a group to control devices in a room")
+async def assign_group_permission(group_id: int, room_id: int):
+    tx = contract.functions.addGroupPermission(group_id, room_id).build_transaction({
+        'gas': 200000,
+        'gasPrice': w3.to_wei('1.05', 'gwei'),
+        'nonce': w3.eth.get_transaction_count(w3.eth.defaultAccount),
+    })
+
+    signed_txn = w3.eth.account.sign_transaction(tx, private_key=private_key)
+
+    t = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+    print(t.hex())
+
+
+@app.get("/check_user_permission", tags=['Permissions'], summary="Check if a user has a permission to control a room")
+async def check_user_permission(user_id: int, room_id: int):
+    return contract.functions.checkUserPermission(user_id, room_id).call()
+
+
+@app.get("/check_group_permission", tags=['Permissions'], summary="Check if a group has a permission to control a room")
+async def check_group_permission(group_id: int, room_id: int):
+    return contract.functions.checkGroupPermission(group_id, room_id).call()
 
 
 class Request(BaseModel):
